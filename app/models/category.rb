123e835ac56bb1d60354d0d3356f1ad03f5ea085
bodrovis <bodrovis@protonmail.com>
2018-05-24 18:20:01 +0300

    graphs

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,4 +7,11 @@ class Category < ApplicationRecord
 
   validates :title, presence: true, length: {minimum: 2}
   validates :description, presence: true, length: {minimum: 2}
+
+  scope :top, -> do
+    select("categories.id, categories.title, count(posts.id) AS posts_count").
+        left_outer_joins(:posts).
+        group("categories.id").
+        order("posts_count DESC")#.limit(5)
+  end
 end
